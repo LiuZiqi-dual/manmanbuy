da8acfe7a51b720b9e3b9854f756d40dd6bdcd90
LiuZiqi-dual <417764848@qq.com>
2018-08-01 22:02:35 +0800

    公共js效果，返回顶部按钮完成

---
+++ js/public.js
@@ -0,0 +1,13 @@
+/* 滚动出顶部区域后显示返回顶部按钮 */
+$(window).on('scroll', function (e) {
+    // console.log($(window).scrollTop());
+    // console.log($(document).height()-$(window).height());
+    if ($(window).scrollTop() == ($(document).height() - $(window).height()) || $(window).scrollTop() == 0) {
+        $('.back_top_btn').show().css({right:'-80px',transform:'rotate(-360deg)'});
+        // $('.back_top_btn').hide();
+    } else {
+        // $('.back_top_btn').show(500);
+        $('.back_top_btn').show().css({right:'85%',transform:'rotate(360deg)'});
+    }
+
+});
