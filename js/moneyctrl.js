d7f2b79061c9debda0e849976a8ec5f71e27bb2a
haihaishuaige <184805761@qq.com>
2018-08-02 23:26:51 +0800

    底部分页功能跳转全部完成

--- js/moneyctrl.js
+++ js/moneyctrl.js
@@ -1,25 +1,56 @@
 //pageid=5   刷新到第几页  41-50   最多到14
-$.get("http://193.112.55.79:9090/api/getmoneyctrl",'',function(res){
+var pageid = pageid || 0;
+ajax();
+function ajax(){
+    $.get("http://193.112.55.79:9090/api/getmoneyctrl",'pageid='+pageid,function(res){
  
-    var num = [];
-    var num2 = [];
-    res.totalCount = Math.floor(res.totalCount/10);
-    for(var i = 0;i<res.result.length;i++){
-         num.push(parseInt(res.result[i].productComCount.replace(/[^0-9]/ig,"")));
-         res.result[i].productComCount = num[i];
-    }      
-    for(var e = 0;e<res.totalCount;e++){
-        num2.push(res.totalCount);
+        var num = [];
+        var num2 = [];
+        res.totalCount = Math.floor(res.totalCount/10);
+        for(var i = 0;i<res.result.length;i++){
+             num.push(parseInt(res.result[i].productComCount.replace(/[^0-9]/ig,"")));
+             res.result[i].productComCount = num[i];
+        }      
+        for(var e = 0;e<res.totalCount+1;e++){
+            num2.push(res.totalCount);      
+        }     
+        //基本数据渲染页面
+        var html = template("template",{data:res.result});     
+        $('.shangping ul').html(html);    
+        // 分页渲染
+        
+        var foot_nav = template("template2",{data:num2});
+        $('#select').html(foot_nav);
+        $('#select option').eq(pageid).prop("selected",true).siblings().prop('selected',false);
+    },'json');
+}
+
+//点击下一页
+$(".foot_next .next").on('click',function(){
+    //判断如果等于最后一页  就不再加载
+    if(pageid==$('#select option').length-1){
+        alert('最后一页了');
+        return;
     }
-    //基本数据渲染页面
-    var html = template("template",{data:res.result});     
-    $('.shangping ul').html(html);    
-    // 分页渲染
-    
-    var foot_nav = template("template2",{data:num2});
-    $('#select').html(foot_nav)
-   
-},'json')
+    pageid+=1;
+    console.log(pageid);      
+    ajax();   
+})
+$(".foot_next .prev").on('click',function(){
+    //判断如果等于最后一页  就不再加载
+    if(pageid==0){
+        alert('这是第一页哦');
+        return;
+    }
+    pageid-=1;
+    console.log(pageid);      
+    ajax();   
+})
+$("#select").on('change',function(){
+    var val = this.value-1;
+    pageid = val;
+    ajax();
+})
 
 
 
