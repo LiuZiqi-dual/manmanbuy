dc9319e37fc4a5241c1a4ec194d944a5af71580e
haihaishuaige <184805761@qq.com>
2018-08-03 16:54:40 +0800

    商品详情内容动态渲染完成

--- js/getmoneyID.js
+++ js/getmoneyID.js
@@ -3,6 +3,10 @@
  getMoney();
  function getMoney(){
      $.get("http://193.112.55.79:9090/api/getmoneyctrlproduct",'productid='+moneyID,function(res){
-            console.log(res.result[0].productComment);
+            // console.log(res.result[0].productCity);
+            //用模板渲染数据
+            var html = template("item",{data:res.result});
+            $(".view").html(html);
+            
      },'json')
  }
