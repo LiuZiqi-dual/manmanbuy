53724e5dc36ddf8280681fac02a126d752e558ba
zhang734804946 <734804946@qq.com>
2018-08-06 13:52:05 +0800

    初始化项目

--- js/getTogetherList.js
+++ js/getTogetherList.js
@@ -1,7 +1,7 @@
 $(function () {
-    
 
-    
+
+
     // (function ($) {
     //     $.getUrlParam = function (name) {
     //         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -13,18 +13,23 @@ $(function () {
     // // 获取id值，调用函数值为要被调取的参数
     // var shopid = $.getUrlParam('');
     // var areaid = $.getUrlParam('');
-    
-    
+
+
     $.ajax({
-        
+
         url: 'http://193.112.55.79:9090/api/getgsproduct',
         type: 'get',
         dataType: 'json',
-        data:{shopid:0,areaid:0},
+        data: {
+            shopid: 0,
+            areaid: 0
+        },
         success: function (res) {
             // console.log(res);
             console.log(res.result);
-            var htmlStr = template("shopListTmp",{data:res.result});
+            var htmlStr = template("shopListTmp", {
+                data: res.result
+            });
             // console.log(htmlStr);
             $('.list-b ul').html(htmlStr);
         }
