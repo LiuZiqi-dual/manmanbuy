b118022c7ef78d21ef5212bb4ceacff8c1cc7071
eterXing <785236579@qq.com>
2018-08-07 08:47:05 +0800

    第二版本

--- js/distop.js
+++ js/distop.js
@@ -11,6 +11,17 @@ var commentimgs = document.querySelector('.commentimgs');
 var commenttitle = document.querySelector('.commenttitle');
 var commentfoot = document.querySelector('.commentfoot');
 // 先获取当前ID的类型
+
+
+// 调用layer的加载部分
+var index = layer.msg('加载中',{
+    time: 10*1000,
+    shade:0.5,
+    shadeClose:false,
+    offset: 'auto',
+    area: '150px'
+}); 
+
 $.ajax({
     url:'http://193.112.55.79:9090/api/getbrandtitle',
     type:'get',
@@ -45,6 +56,7 @@ $.ajax({
         // console.log(arr);
         var htmlStr = template('racktmp',{data:arr});
         rackul.innerHTML = htmlStr;
+        layer.close(index);
     }
 })
 
@@ -66,7 +78,7 @@ $.ajax({
         var htmlStr = template('allbrands',{data:arr});
         allbrands.innerHTML = htmlStr;
 
-        console.log(productid);
+        // console.log(productid);
         //在后面获取
         //商品评论
         $.ajax({
@@ -75,6 +87,7 @@ $.ajax({
     dataType:'json',
     data:{'productid':productid},
     success:function(res){
+        
         var arr = res.result;
         var htmlStr = template('talkcomment',{data:arr});
         // console.log(htmlStr);
@@ -91,7 +104,7 @@ $.ajax({
 changesize();
 function changesize(){
     var htmlsize = 100;
-    var htmlwidth = 1400;
+    var htmlwidth = 500;
     var newwidth = document.querySelector('html').offsetWidth;
     var newsize = newwidth*htmlsize/htmlwidth;
     document.querySelector('html').style.fontSize = newsize+'px';
