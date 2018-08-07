57245c9c2608027c434dcaf4b3b553f028c9f646
haihaishuaige <184805761@qq.com>
2018-08-07 10:20:31 +0800

    二级菜单渲染完成

--- js/public.js
+++ js/public.js
@@ -10,3 +10,33 @@ $(window).on('scroll', function (e) {
         $('.back_top_btn').show().css({right:'10px',transform:'rotate(360deg)'});
     }
 });
+//search
+// if($(".search buton"))
+// var button = $(".search_box button")[0];
+// var flag;
+// button.touchstart = function(e){
+//     flag=true;
+//     //求出鼠标的相对位置
+//    temp= e.pageY-outer.offsetTop-slider.offsetTop;
+// }
+// document.ontouchend=function(){
+//     flag=false;
+// }
+// document.ontouchmove=function(e){
+//     if(flag){
+//       //求出滚轮对于盒子顶部的距离
+//         var y= e.pageY-outer.offsetTop-temp;
+//         if(y<0)y=0;
+//         var maxY=outer.offsetHeight-slider.offsetHeight-2;
+//         if(y>maxY)y=maxY;
+//         slider.style.top=y+"px";
+//         //-----------------------
+//         //求出内容最大距离  ==  内容高度-容器高度
+//         var neirong=inner.offsetHeight-outer.offsetHeight;
+//         var innerH=y*neirong/maxY;
+//         inner.style.top=-innerH+"px";
+//     }
+// }
+
+
+// console.log($(".search buton").pageY);
