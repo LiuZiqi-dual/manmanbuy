874a1ecde47c73f9c1ea657dc9272ab6d39b97b7
LiuZiqi-dual <417764848@qq.com>
2018-08-07 10:00:24 +0800

    商品列表页面完成，跳转商品详情页面测试

--- js/productlist.js
+++ js/productlist.js
@@ -1,10 +1,33 @@
+var categoryid = location.search.substr(12);
+console.log(categoryid);
+
 $.ajax({
     type: "get",
     url: "http://193.112.55.79:9090/api/getcategorybyid",
-    
+    data:"categoryid="+categoryid,
     dataType: "json",
     success: function (res) {
         console.log(res.result);
-        
+        $('.menutree .menu .temp_category a').html(
+            template(
+                'menutree_template',{
+                    res:res.result
+                }
+            )
+        );
+    }
+});
+$.ajax({
+    type: "get",
+    url: "http://193.112.55.79:9090/api/getproductlist",
+    data: "categoryid="+categoryid,
+    dataType: "json",
+    success: function (data) {
+        console.log(data.result);
+        $('.container ul').html(template(
+            'productlist_template',{
+                data:data.result
+            }
+        ));
     }
 });
