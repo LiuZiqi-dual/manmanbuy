40e1590b1ebc040558c92bdf7c9bab2dc09a9a97
zhang734804946 <734804946@qq.com>
2018-08-07 22:36:28 +0800

    修复优惠券页面轮播图数据渲染

--- js/getcouponproduct.js
+++ js/getcouponproduct.js
@@ -1,15 +1,14 @@
 $(function () {
     (function ($) {
         $.getUrlParam = function (name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) return unescape(r[2]);
-        return null;
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return unescape(r[2]);
+            return null;
         }
-        })(jQuery);
-        // 获取id值，调用函数值为要被调取的参数
-        var id = $.getUrlParam('couponId');
-
+    })(jQuery);
+    // 获取id值，调用函数值为要被调取的参数
+    var id = $.getUrlParam('couponId');
     $.ajax({
         url: 'http://193.112.55.79:9090/api/getcouponproduct',
         type: 'get',
@@ -19,10 +18,11 @@ $(function () {
         },
         success: function (res) {
             console.log(res);
+
             var htmlStr = template('getcouponproductTmp', {
                 data: res.result
             });
-            console.log(htmlStr);
+            // console.log(htmlStr);
             $('.view ul').html(htmlStr);
         }
     })
